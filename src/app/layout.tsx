2680c97b69938c19d022218259dc11c13fccb6ee
fjmillan32-bit <fjmillan38@gmail.com>
2026-06-18 13:40:59 -0500

    fix: Iconos PWA, boton salir solo Dashboard, middleware admin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 ﻿import type { Metadata } from "next";
-import LogoutButton from '@/components/shared/LogoutButton'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -23,9 +22,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <LogoutButton />
+        
         {children}
       </body>
     </html>
   );
 }
+
