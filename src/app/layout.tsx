9ed6ab3481a0c7159bdfa2fd41e07a93cddd8b81
fjmillan32-bit <fjmillan38@gmail.com>
2026-06-22 11:51:11 -0500

    fix: Meta tags PWA modernos (mobile-web-app-capable)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="es"><head><link rel="icon" href="/favicon.ico" /><link rel="apple-touch-icon" href="/favicon.ico" /><meta name="apple-mobile-web-app-capable" content="yes" /><meta name="apple-mobile-web-app-title" content="SIGEA" /></head>
+    <html lang="es"><head><link rel="icon" href="/favicon.ico" /><link rel="apple-touch-icon" href="/favicon.ico" /><meta name="mobile-web-app-capable" content="yes" /><meta name="application-name" content="SIGEA" /></head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         
         {children}
@@ -28,6 +28,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   );
 }
+
 
 
 
