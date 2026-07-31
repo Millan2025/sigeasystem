9f6528ae7d567d7174e1d56b46c6617d44a40466
fjmillan32-bit <fjmillan38@gmail.com>
2026-07-31 13:14:04 -0500

    fix: crear [slug]/page.tsx

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import NegocioHome from "@/components/NegocioHome";
+
+interface Props {
+  params: Promise<{ slug: string }>
+  searchParams: Promise<{ tenant?: string }>
+}
+
+export default async function NegocioPage({ params, searchParams }: Props) {
+  const { slug } = await params;
+  return <NegocioHome negocioSlug={slug} />;
+}
